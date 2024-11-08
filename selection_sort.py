8291cdc16f315f51cd720c845d18db0a8e916f96
cemenkawhopper <deadinside0088@gmail.com>
2024-11-08 09:25:00 +0700

    selection_sort

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+import random
+
+def selection_sort(nums):
+    for i in range(len(nums)):
+        lowest_value_index = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[lowest_value_index]:
+                lowest_value_index = j
+        nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
+
+nums = []
+for i in range(random.randint(5, 100)):
+    nums.append(random.randint(5, 100))
+print(nums)
+start = time.perf_counter()
+end = time.perf_counter()
+print(end-start)
